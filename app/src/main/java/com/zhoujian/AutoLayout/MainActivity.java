595c8d91f169c7e54d305a919018870930b94779
zeke123 <zekezhoujian@163.com>
2017-07-29 18:43:38 +0800

    Initial commit

---
+++ app/src/main/java/com/zhoujian/AutoLayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhoujian.AutoLayout;
+
+import android.os.Bundle;
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class MainActivity extends AutoLayoutActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
